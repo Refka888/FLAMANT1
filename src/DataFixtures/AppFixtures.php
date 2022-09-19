595c600fecaee89cc0fffb47279021c40e966c09
Refka <refka.gmati@gmail.com>
2022-09-19 10:31:26 +0100

    insertion react js

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,10 +5,12 @@ namespace App\DataFixtures;
 use App\Entity\Cat;
 use App\Entity\User;
 use App\Entity\Order;
+use DateTimeImmutable;
 use App\Entity\Product;
 use App\Entity\UserAuth;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -22,17 +24,27 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
          // Création d'un user "normal"
-         $userAuth = new UserAuth();
+         $userAuth = new User();
+         $userAuth ->setfirstName("User");
+         $userAuth ->setlastName("Client");
          $userAuth->setEmail("user@venteapi.com");
+         $userAuth ->setPhone("2233445");
          $userAuth->setRoles(["ROLE_USER"]);
          $userAuth->setPassword($this->userPasswordHasher->hashPassword($userAuth, "password"));
+         $userAuth->setCreatedAt(new DateTimeImmutable());
+         $userAuth->setUpdatedAt(new DateTimeImmutable());
          $manager->persist($userAuth);
          
          // Création d'un user admin
-         $userAdmin = new UserAuth();
+         $userAdmin = new User();
+         $userAdmin ->setfirstName("Admin");
+            $userAdmin ->setlastName("Admin");
+            $userAdmin->setPhone('1234567890');
          $userAdmin->setEmail("admin@venteapi.com");
          $userAdmin->setRoles(["ROLE_ADMIN"]);
          $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
+         $userAdmin->setCreatedAt(new DateTimeImmutable());
+         $userAdmin->setUpdatedAt(new DateTimeImmutable());
          $manager->persist($userAdmin);
         $listUser = [];
         for ($i=0; $i < 20; $i++){
@@ -40,8 +52,11 @@ class AppFixtures extends Fixture
             $user ->setfirstName("firstName". $i);
             $user ->setlastName("lastName". $i);
             $user ->setemail("user". $i ."@gmail.com");
-            $user ->setpassword("**********");
+            $user ->setpassword($this->userPasswordHasher->hashPassword($userAuth, "password"));
             $user ->setPhone("2233445". $i);
+            $user->setRoles(["ROLE_USER"]);
+            $user->setCreatedAt(new DateTimeImmutable());
+            $user->setUpdatedAt(new DateTimeImmutable());
             $manager ->persist($user);
             $listUser[] = $user;
          }
@@ -52,6 +67,8 @@ class AppFixtures extends Fixture
              $order ->setCode("123456". $i);
              $order ->setStatut($listStatut[array_rand($listStatut)]);
              $order ->setUsers($listUser[array_rand($listUser)]);
+             $order->setCreatedAt(new DateTimeImmutable());
+             $order->setUpdatedAt(new DateTimeImmutable());
              $manager ->persist($order);
              $listOrder[] = $order;
          }
@@ -61,6 +78,8 @@ class AppFixtures extends Fixture
          for ($i=0; $i < 20; $i++){
             $cat = new Cat();
             $cat ->setName("Category". $i);
+            $cat->setCreatedAt(new DateTimeImmutable());
+            $cat->setUpdatedAt(new DateTimeImmutable());
             $manager ->persist($cat);
             $listCat[] = $cat;
          }
@@ -76,6 +95,8 @@ class AppFixtures extends Fixture
             $product ->setPrice($listQuantity[array_rand($listQuantity)]);
             $product ->setOrders($listOrder[array_rand($listOrder)]);
             $product ->setCategory($listCat[array_rand($listCat)]);
+            $product->setCreatedAt(new DateTimeImmutable());
+            $product->setUpdatedAt(new DateTimeImmutable());
             $manager ->persist($product);
 
         }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,12 +8,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
-
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\HasLifecycleCallbacks]
 
-class User
+class User  implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use Timer;
     #[ORM\Id]
@@ -44,6 +45,9 @@ class User
 
     #[ORM\OneToMany(mappedBy: 'users', targetEntity: Order::class)]
     private Collection $orders;
+
+    #[ORM\Column]
+    private array $roles = [];
 
     public function __construct()
     {
@@ -147,4 +151,49 @@ class User
         return $this;
     }
 
+/**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+      /**
+     * Méthode getUsername qui permet de retourner le champ qui est utilisé pour l'authentification.
+     *
+     * @return string
+     */
+    public function getUsername(): string {
+        return $this->getUserIdentifier();
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 }
